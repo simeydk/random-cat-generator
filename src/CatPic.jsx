e3515f2e99282ce36dea90ad9ce609d7b0975dce
Simey de Klerk <simeydeklerk@outlook.com>
2019-05-02 23:12:51 +0200

    cache images v0

--- src/CatPic.jsx
+++ src/CatPic.jsx
@@ -2,14 +2,31 @@ import React, { useState, useEffect } from "react";
 import { getCatUrl } from "./api";
 import LoaderImg from "./LoaderImg";
 import Spinner from "./Spinner";
-
 import "./cats.css";
+
+const imageCache = []
+
+const cacheImage = (src) => {
+  const img = new Image()
+  img.src = src
+  imageCache.push(img)
+} 
+
+ const cacheCat = async (n = 1) => {
+  for (let i = 0; i < n; i++) {
+    cacheImage(await getCatUrl())
+  }
+}
+
+cacheCat(5)
 
 const CatPic = () => {
   const [url, setUrl] = useState(null);
 
   const getNewCat = async () => {
-    const url = await getCatUrl();
+    cacheCat()
+    const url = imageCache.shift().src || await getCatUrl();
+    // const url = await getCatUrl();
     setUrl(url);
   };
 
