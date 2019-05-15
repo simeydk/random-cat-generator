3fbb306c4bfdd7d45124e125dcf4efdf0a6783b7
Simey de Klerk <simeydeklerk@outlook.com>
2019-05-15 10:00:22 +0200

    Add LoadingPlaceholder to useRefresh

--- src/useRefresh.jsx
+++ src/useRefresh.jsx
@@ -1,9 +1,10 @@
 import {useState, useEffect} from 'react'
 
-function useRefresh(generatorFunction, initialValue = undefined) {
+function useRefresh(generatorFunction, initialValue = undefined, loadingPlaceholder = undefined) {
   const [result, setResult] = useState(initialValue)
 
   async function refresh() {
+    if (loadingPlaceholder !== undefined) await setResult(loadingPlaceholder)
     const newResult = await generatorFunction()
     setResult(newResult)
   }
